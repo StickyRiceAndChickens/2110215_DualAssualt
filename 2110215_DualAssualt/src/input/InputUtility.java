7b348a4d0ca9239cbad3c8ad7f94b62755e73fdb
Nithirun Numnonda <nithirun1995@gmail.com>
2015-12-08 21:14:24 +0700

    add inputUtility

---
+++ 2110215_DualAssualt/src/input/InputUtility.java
@@ -0,0 +1,116 @@
+package input;
+
+public class InputUtility {
+	//to check status of input
+	private static int mouseX, mouseY;
+	private static boolean mouseLeftDown, mouseRightDown, mouseOnScreen;
+	private static boolean mouseLeftTriggered, mouseRightTriggered;
+	private static boolean[] keyPressed = new boolean[256];
+	private static boolean[] keyTriggered = new boolean[256];
+
+	public static int getMouseX() {
+		return mouseX;
+	}
+
+	public static void setMouseX(int mouseX) {
+		InputUtility.mouseX = mouseX;
+	}
+
+	public static int getMouseY() {
+		return mouseY;
+	}
+
+	public static void setMouseY(int mouseY) {
+		InputUtility.mouseY = mouseY;
+	}
+
+	public static boolean isMouseLeftDown() {
+		return mouseLeftDown;
+	}
+
+	public static void setMouseLeftDown(boolean mouseLeftDown) {
+		InputUtility.mouseLeftDown = mouseLeftDown;
+	}
+
+	public static boolean isMouseRightDown() {
+		return mouseRightDown;
+	}
+
+	public static void setMouseRightDown(boolean mouseRightDown) {
+		InputUtility.mouseRightDown = mouseRightDown;
+	}
+
+	public static boolean isMouseOnScreen() {
+		return mouseOnScreen;
+	}
+
+	public static void setMouseOnScreen(boolean mouseOnScreen) {
+		InputUtility.mouseOnScreen = mouseOnScreen;
+	}
+
+	public static boolean isMouseLeftTriggered() {
+		return mouseLeftTriggered;
+	}
+
+	public static boolean isMouseRightTriggered() {
+		return mouseRightTriggered;
+	}
+
+	public static void setMouseLeftTriggered(boolean v) {
+
+		mouseLeftTriggered = v;
+	}
+
+	public static boolean isMouseLeftClicked() {
+		return mouseLeftTriggered;
+
+	}
+
+	public static boolean isMouseRightClicked() {
+		return mouseRightTriggered;
+
+	}
+
+	public static void setMouseRightTriggered(boolean v) {
+		mouseRightTriggered = v;
+	}
+
+	public static boolean getKeyPressed(int key) {
+		if (key < 0 || key > 255)
+			return false;
+		return keyPressed[key];
+	}
+
+	public static void setKeyPressed(int key, boolean pressed) {
+
+		if (key < 0 || key > 255)
+			return;
+		keyPressed[key] = pressed;
+	}
+
+	public static boolean getKeyTriggered(int key) {
+
+		if (key < 0 || key > 255)
+			return false;
+		return keyTriggered[key];
+	}
+
+	public static void setKeyTriggered(int key, boolean pressed) {
+		if (key < 0 || key > 255)
+			return;
+		if (!keyPressed[key])
+			keyTriggered[key] = pressed;
+	}
+
+	public static void postUpdate() {
+		/* setting before update */
+		mouseLeftTriggered=false;
+		mouseRightTriggered=false;
+		for (int i = 0; i < 256; i++) {
+			keyTriggered[i]=false;
+		}
+		
+		
+	}
+}
+
