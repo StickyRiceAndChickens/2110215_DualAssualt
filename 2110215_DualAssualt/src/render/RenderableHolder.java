8194264e8b9071f57e8bfabf9dc7ed234f024db5
Nithirun Numnonda <nithirun1995@gmail.com>
2015-12-08 21:03:12 +0700

    new Project add Irenderalbe class

---
+++ 2110215_DualAssualt/src/render/RenderableHolder.java
@@ -0,0 +1,35 @@
+package render;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class RenderableHolder {
+	private static final RenderableHolder instance = new RenderableHolder();
+	private List<IRenderable> entities;
+	
+	public static RenderableHolder getInstance(){
+		return instance;
+	}
+	
+	public RenderableHolder(){
+		entities = new ArrayList<IRenderable>();
+	}
+	
+	public void add(IRenderable entity){
+		entities.add(entity);
+		//Sort our list by Z -- we don't sort during "image drawing" as it's not efficient
+		Collections.sort(entities, new Comparator<IRenderable>() {
+			@Override
+			public int compare(IRenderable o1, IRenderable o2) {
+				if(o1.getZ() > o2.getZ()) return 1;
+				return -1;
+			}
+		});
+	}
+	
+	public List<IRenderable> getRenderableList(){
+		return entities;
+	}
+}
