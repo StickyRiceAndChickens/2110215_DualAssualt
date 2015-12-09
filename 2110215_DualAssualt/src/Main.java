9dca67843cacfebf553861d5df695d518010d7f2
Nithirun Numnonda <nithirun1995@gmail.com>
2015-12-09 22:38:38 +0700

    update main

--- 2110215_DualAssualt/src/Main.java
+++ 2110215_DualAssualt/src/Main.java
@@ -1,9 +1,12 @@
+import logic.GameLogic;
+import render.GameManager;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		GameLogic logic = new GameLogic();
+		GameManager.runGame(logic);
 	}
 
 }
