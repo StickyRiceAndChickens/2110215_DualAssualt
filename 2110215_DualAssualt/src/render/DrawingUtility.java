58a3164fadc9b5048e13bd99cb41e21db8b497b3
Ascharapon Sinlamuth <greatlife224@gmail.com>
2015-12-12 19:38:06 +0700

    TitleScreenPerfect

--- 2110215_DualAssualt/src/render/DrawingUtility.java
+++ 2110215_DualAssualt/src/render/DrawingUtility.java
@@ -1,5 +1,6 @@
 package render;
 
+import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 
 import javax.imageio.ImageIO;
@@ -23,7 +24,26 @@ public class DrawingUtility {
 
 	}
 	/*install image*/
-	protected static final BufferedImage bg = getImage("res/img/bgTitle.jpg");
+	protected static final BufferedImage titleBG = getImage("res/img/bgTitle.png");
 	public static final BufferedImage playerImage = getImage("res/img/charingame3.png");
 	public static BufferedImage playerProfile = getImage("res/img/profile1.jpg");
+	
+	public static BufferedImage resizeImage(BufferedImage originalImage, int type) {
+		int newHeight = (originalImage.getHeight()*SettingScreen.screenHeight)/700;
+		int newWidht = (originalImage.getWidth()*SettingScreen.screenWidth)/1245;
+		BufferedImage resizedImage = new BufferedImage(newWidht, newHeight, type);
+		Graphics2D g = resizedImage.createGraphics();
+		g.drawImage(originalImage, 0, 0, newWidht, newHeight, null);
+		g.dispose();
+		return resizedImage;
+	}
+	
+	public static BufferedImage resizeButton(BufferedImage originalImage, int type) {
+		BufferedImage resizedImage = new BufferedImage(originalImage.getWidth(), originalImage.getWidth(), type);
+		Graphics2D g = resizedImage.createGraphics();
+		g.drawImage(originalImage, 0, 0, originalImage.getWidth()-180, originalImage.getHeight()-40, null);
+		g.dispose();
+		return resizedImage;
+	}
+	
 }
---
+++ 2110215_DualAssualt/src/render/TitleBackground.java
@@ -0,0 +1,41 @@
+package render;
+
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+
+public class TitleBackground implements IRenderable{
+	
+	private BufferedImage titleBG = null;
+	public static int imageWidth;
+	public static int imageHeight;
+	
+	public TitleBackground(){
+		this.titleBG = resizeImage(DrawingUtility.titleBG, 1);
+	}
+	
+	public static BufferedImage resizeImage(BufferedImage originalImage, int type) {
+		BufferedImage resizedImage = new BufferedImage(SettingScreen.screenWidth, SettingScreen.screenHeight, type);
+		Graphics2D g = resizedImage.createGraphics();
+		g.drawImage(originalImage, 0, 0, SettingScreen.screenWidth-300, SettingScreen.screenHeight, null);
+		g.dispose();
+		return resizedImage;
+	}
+	
+	
+	public void draw(Graphics2D g2d) {
+		g2d.drawImage(titleBG, null, 300, 0);
+	}
+
+	@Override
+	public int getZ() {
+		// TODO Auto-generated method stub
+		return Integer.MIN_VALUE;
+	}
+
+	@Override
+	public boolean isVisible() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+}
