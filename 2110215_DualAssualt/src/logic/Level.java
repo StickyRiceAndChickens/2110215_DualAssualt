ae7819c361c177fadeafcd7417464887f37ba41c
Ascharapon Sinlamuth <greatlife224@gmail.com>
2015-12-16 04:12:00 +0700

    Level

--- 2110215_DualAssualt/src/logic/Level.java
+++ 2110215_DualAssualt/src/logic/Level.java
@@ -4,6 +4,7 @@ import java.awt.image.BufferedImage;
 import java.util.Set;
 
 import render.DrawingUtility;
+import render.GameManager;
 import render.RenderableHolder;
 import render.SettingScreen;
 
@@ -11,8 +12,8 @@ public class Level {
 	private Enemy e1;
 	private Gun gun1;
 	private int enemyCount = 0;
-	private int x[] = {200,220,220,220,400,800,800,800,1100,1100,1100,80};
-	private int y[] = {40,200,230,500,40,500,40,200,500,40,200,400,40};
+	private int x[] = {200,220,220,220,450,650,800,650,1100,1100,700,120};
+	private int y[] = {100,200,300,500,100,500,100,200,100,200,400,500};
 	private BufferedImage imageChar[] = { DrawingUtility.enemy1, DrawingUtility.enemy1, DrawingUtility.enemy1,
 			DrawingUtility.enemy1, DrawingUtility.enemy1, DrawingUtility.enemy1, DrawingUtility.enemy2,
 			DrawingUtility.enemy2, DrawingUtility.enemy2, DrawingUtility.enemy2, DrawingUtility.enemy2,
@@ -30,46 +31,49 @@ public class Level {
 			enemyCount++;
 		}
 		
-		wall = new MapObject(70, 300, 10, 500);
+		wall = new MapObject(70, 300, 20, 500);
 		RenderableHolder.getInstance().add(wall);
 		GameLogic.map.addEntity(wall);
 		
-		wall = new MapObject(SettingScreen.screenWidth/2, 50, 1120, 10);
+		wall = new MapObject(SettingScreen.screenWidth/2, 50, 1120, 20);
 		RenderableHolder.getInstance().add(wall);
 		GameLogic.map.addEntity(wall);
 		
-		wall = new MapObject(1200, 230, 10, 380);
+		wall = new MapObject(1200, 230, 20, 380);
 		RenderableHolder.getInstance().add(wall);
 		GameLogic.map.addEntity(wall);
 		
-		wall = new MapObject(500, 550, 850, 10);
+		wall = new MapObject(500, 550, 850, 20);
 		RenderableHolder.getInstance().add(wall);
 		GameLogic.map.addEntity(wall);
 		
-		wall = new MapObject(300, 350, 10, 400);
+		wall = new MapObject(300, 350, 20, 400);
 		RenderableHolder.getInstance().add(wall);
 		GameLogic.map.addEntity(wall);
 		
-		wall = new MapObject(400, 250, 10, 400);
+		wall = new MapObject(400, 250, 20, 400);
 		RenderableHolder.getInstance().add(wall);
 		GameLogic.map.addEntity(wall);
 		
-		wall = new MapObject(500, 350, 10, 400);
+		wall = new MapObject(500, 350, 20, 400);
 		RenderableHolder.getInstance().add(wall);
 		GameLogic.map.addEntity(wall);
 		
-		wall = new MapObject(600, 250, 10, 400);
+		wall = new MapObject(600, 250, 20, 400);
 		RenderableHolder.getInstance().add(wall);
 		GameLogic.map.addEntity(wall);
 		
-		wall = new MapObject(800, 350, 10, 400);
+		wall = new MapObject(800, 350, 20, 400);
 		RenderableHolder.getInstance().add(wall);
 		GameLogic.map.addEntity(wall);
 		
-		wall = new MapObject(1070, 420, 270, 10);
+		wall = new MapObject(1070, 420, 270, 20);
 		RenderableHolder.getInstance().add(wall);
 		GameLogic.map.addEntity(wall);
 		
-		
+		GameManager.p1.setX(1100);
+		GameManager.p1.setY(500);
+		GameManager.p2.setX(1200);
+		GameManager.p2.setY(500);
 	}
 }
