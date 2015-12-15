234513741cfb09991fadd41170923bbe9c41bff4
Ascharapon Sinlamuth <greatlife224@gmail.com>
2015-12-16 03:00:08 +0700

    update level

--- 2110215_DualAssualt/src/logic/Level.java
+++ 2110215_DualAssualt/src/logic/Level.java
@@ -9,8 +9,8 @@ public class Level {
 	private Enemy e1;
 	private Gun gun1;
 	private int enemyCount = 0;
-	private int x[] = {200,220,220,220,400,800,800,800,1100,1100,1100};
-	private int y[] = {40,200,230,700,40,550,40,200,600,40,200,400};
+	private int x[] = {200,220,220,220,400,800,800,800,1100,1100,1100,40};
+	private int y[] = {40,200,230,700,40,550,40,200,600,40,200,400,40};
 	private BufferedImage imageChar[] = { DrawingUtility.enemy1, DrawingUtility.enemy1, DrawingUtility.enemy1,
 			DrawingUtility.enemy1, DrawingUtility.enemy1, DrawingUtility.enemy1, DrawingUtility.enemy2,
 			DrawingUtility.enemy2, DrawingUtility.enemy2, DrawingUtility.enemy2, DrawingUtility.enemy2,
@@ -25,7 +25,6 @@ public class Level {
 			GameLogic.map.addEntity(e1);
 			GameLogic.map.addEntity(gun1);
 			enemyCount++;
-
 		}
 	}
 }
