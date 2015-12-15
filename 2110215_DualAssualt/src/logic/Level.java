ba831b56961bae9e9806d9dafc203f342b47fb85
Nithirun Numnonda <nithirun1995@gmail.com>
2015-12-15 17:00:51 +0700

    add level

---
+++ 2110215_DualAssualt/src/logic/Level.java
@@ -0,0 +1,29 @@
+package logic;
+
+import render.DrawingUtility;
+import render.RenderableHolder;
+
+public class Level {
+	private Enemy e1;
+	private Gun gun1;
+	private int enemyCount = 0;
+
+	public Level() {
+		int x = 500;
+		int y = 100;
+		while (enemyCount < 12) {
+			e1 = new Enemy(30, x, y, 70, 40, 180, null, 3, DrawingUtility.enemy1);
+			gun1 = new Gun(x, y, 0, e1, 3);
+			e1.setWeapon(gun1);
+			RenderableHolder.getInstance().add(e1);
+			GameLogic.map.addEntity(e1);
+			GameLogic.map.addEntity(gun1);
+			enemyCount++;
+			y += 100;
+			if (y > 500) {
+				y = 100;
+				x -= 100;
+			}
+		}
+	}
+}
